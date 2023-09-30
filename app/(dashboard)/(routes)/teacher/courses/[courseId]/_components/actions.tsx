4e877d0088d947f53f94dd4715d04da5ce4bfda5
Dangis <dangiskazukauskas@gmail.com>
2023-09-30 23:04:42 +0300

    course publishing, confetti on publishing

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/actions.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import { ConfirmModal } from "@/components/modals/confirm-modal";
+import { Button } from "@/components/ui";
+import { useConfettiStore } from "@/hooks/use-confetti-store";
+import axios from "axios";
+import { Trash } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import toast from "react-hot-toast";
+
+export const Actions = ({ disabled, courseId, isPublished }: ActionsProps) => {
+  const router = useRouter();
+  const confetti = useConfettiStore();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onPublishChange = async () => {
+    try {
+      setIsLoading(true);
+      if (isPublished) {
+        await axios.patch(`/api/courses/${courseId}/unpublish`);
+        toast.success("Course unpublished!");
+      } else {
+        await axios.patch(`/api/courses/${courseId}/publish`);
+        toast.success("Course published!");
+        confetti.onOpen();
+      }
+      router.refresh();
+    } catch {
+      toast.error("Something went wrong");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const onDelete = async () => {
+    try {
+      setIsLoading(true);
+      await axios.delete(`/api/courses/${courseId}`);
+      toast.success("Course deleted!");
+      router.refresh();
+      router.push(`/teacher/courses`);
+    } catch {
+      toast.error("Something went wrong");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  return (
+    <div className="flex items-center gap-x-2">
+      <Button
+        onClick={onPublishChange}
+        disabled={disabled || isLoading}
+        variant="outline"
+        size="sm"
+      >
+        {isPublished ? "Unpublish" : "Publish"}
+      </Button>
+      <ConfirmModal onConfirm={onDelete}>
+        <Button size="sm" disabled={isLoading}>
+          <Trash className="w-4 h-4" />
+        </Button>
+      </ConfirmModal>
+    </div>
+  );
+};
+
+type ActionsProps = {
+  disabled: boolean;
+  courseId: string;
+  isPublished: boolean;
+};
